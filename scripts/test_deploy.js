19abb5f7bee77aa3cdbd04cf326ac17aaad6db6e
AntonLED <al3406099@gmail.com>
2025-12-18 18:58:42 +0300

    [upd] Updated deploy code and .gitignore

--- scripts/test_deploy.js
+++ scripts/test_deploy.js
@@ -11,30 +11,13 @@ async function main() {
   const token = await MockToken.connect(deployer).deploy();
   await token.waitForDeployment();
   const tokenAddress = await token.getAddress();
-  console.log("MockToken contract deployed\n");
+  console.log("MockToken contract deployed at ", tokenAddress);
 
   const SubscriptionCore = await hre.ethers.getContractFactory("SubscriptionCore");
   const core = await SubscriptionCore.connect(deployer).deploy();
   await core.waitForDeployment();
   const coreAddress = await core.getAddress();
-  console.log("SubscriptionCore contract deployed\n");
-
-  const amountToBuyer = hre.ethers.parseUnits("5000", 18);
-  
-  await token.connect(deployer).transfer(buyer.address, amountToBuyer);
-
-  console.log(`Deplyer --> Buyer (${hre.ethers.formatUnits(amountToBuyer, 18)} tokens)`);
-  console.log(`Byer balance: ${hre.ethers.formatUnits(await token.balanceOf(buyer.address), 18)} USDT\n`);
-  
-  const planPrice = hre.ethers.parseUnits("100", 18);
-  const planFreq = 60; 
-  
-  await core.connect(merchant).createPlan(tokenAddress, planPrice, planFreq);
-
-  console.log("Merchant creates a subscription plan:");
-  console.log(`Price: ${hre.ethers.formatUnits(planPrice, 18)} tokerns`);
-  console.log(`Frequency: ${planFreq} sec`);
-  console.log(`Merchant addr: ${merchant.address}\n`);
+  console.log("SubscriptionCore contract deployed at ", coreAddress);
 }
 
 main().catch((error) => {
